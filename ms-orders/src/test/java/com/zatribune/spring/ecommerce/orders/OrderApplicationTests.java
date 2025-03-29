ec6aed52f6be6be58c3ebbfdad0db999040ed752
kinoushamaye <kinousha34@gmail.com>
2025-03-28 23:50:16 -0400

    fix pipeline

--- ms-orders/src/test/java/com/zatribune/spring/ecommerce/orders/OrderApplicationTests.java
+++ ms-orders/src/test/java/com/zatribune/spring/ecommerce/orders/OrderApplicationTests.java
@@ -2,12 +2,13 @@ package com.zatribune.spring.ecommerce.orders;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Charge application-test.yml
 class OrderApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
