db1497ade174a7bc2694f86ba44a16f4d3bd176b
kinoushamaye <kinousha34@gmail.com>
2025-03-30 22:39:15 -0400

    fix problem

--- ms-orders/src/main/java/com/zatribune/spring/ecommerce/orders/OrderApplication.java
+++ ms-orders/src/main/java/com/zatribune/spring/ecommerce/orders/OrderApplication.java
@@ -2,11 +2,12 @@ package com.zatribune.spring.ecommerce.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@EnableKafkaStreams
+@EnableKafka
 @SpringBootApplication
 public class OrderApplication {
 
