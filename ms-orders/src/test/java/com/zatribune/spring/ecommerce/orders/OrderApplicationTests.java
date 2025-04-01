fb2b11af09e5b00aca4766ed1609b6ddc5a75221
kinoushamaye <kinousha34@gmail.com>
2025-03-31 20:26:55 -0400

    fix problem 5

--- ms-orders/src/test/java/com/zatribune/spring/ecommerce/orders/OrderApplicationTests.java
+++ ms-orders/src/test/java/com/zatribune/spring/ecommerce/orders/OrderApplicationTests.java
@@ -1,13 +1,15 @@
-package com.zatribune.spring.ecommerce.orders;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(
+    topics = {"orders"},
+    partitions = 1,
+    brokerProperties = {"listeners=PLAINTEXT://localhost:9092", "port=9092"}
+)
 class OrderApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
