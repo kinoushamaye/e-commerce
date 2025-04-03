e772d4ac7deae89d95678e63c4dcb28da75b986a
kinoushamaye <kinousha34@gmail.com>
2025-04-02 21:32:26 -0400

    fixer

--- ms-payment/src/test/java/com/zatribune/spring/ecommerce/payments/PaymentApplicationTests.java
+++ ms-payment/src/test/java/com/zatribune/spring/ecommerce/payments/PaymentApplicationTests.java
@@ -3,13 +3,16 @@ package com.zatribune.spring.ecommerce.payments;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+
 class PaymentApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+   
 
 }
